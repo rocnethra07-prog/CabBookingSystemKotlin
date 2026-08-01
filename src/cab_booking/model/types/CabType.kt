d5435b9a034ad0446136e3e8e81fc5fcd8464372
rocnethra07-prog <rocnethra07@gmail.com>
2026-08-01 19:56:07 +0530

    Commit

--- src/cab_booking/model/types/CabType.kt
+++ src/cab_booking/model/types/CabType.kt
@@ -3,7 +3,7 @@ package cab_booking.model.types
 import java.math.BigDecimal
 
 enum class CabType(val basePay: BigDecimal, val perKmRate: BigDecimal) {
-    MINI(BigDecimal(40), BigDecimal("8.0")),
+    MINI(BigDecimal(40), BigDecimal(8)),
 
     SEDAN(BigDecimal(60), BigDecimal("11.0")),
 
