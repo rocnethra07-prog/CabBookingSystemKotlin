733a4903c737f3ea18a955dd5eb5bca4c7b7afcb
rocnethra07-prog <rocnethra07@gmail.com>
2026-08-11 11:29:19 +0530

    Commit

--- src/cab_booking/auth/UserCredential.kt
+++ src/cab_booking/auth/UserCredential.kt
@@ -15,13 +15,7 @@ class UserCredential(val userId: String, password: String) {
 
     private var passwordHash: String
     private var failedAttempts: Int = 0
-
-//    val isAccountLocked: Boolean
-//        get() {
-//            refreshLockStatus()
-//            return lockedAt != null
-//        }
-
+    
     fun isAccountLocked() : Boolean {
         refreshLockStatus()
         return lockedAt != null
