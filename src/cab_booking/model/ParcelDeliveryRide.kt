7db665a3ba37317fea2837c852dd229f203c73c5
rocnethra07-prog <rocnethra07@gmail.com>
2026-08-24 16:30:57 +0530

    Commit

--- src/cab_booking/model/ParcelDeliveryRide.kt
+++ src/cab_booking/model/ParcelDeliveryRide.kt
@@ -5,22 +5,22 @@ import cab_booking.model.types.ParcelDeliveryType
 import cab_booking.util.Validator
 import java.math.BigDecimal
 
-class ParcelRide(
+class ParcelDeliveryRide(
      riderId: String,
      driverId: String,
      pickupLocation: Location,
      dropLocation: Location,
      fare: BigDecimal,
      val parcelId : String,
-     val contactName: String,
-     val contactPhone: String,
+     val contactPersonName: String,
+     val contactPersonPhoneNumber: String,
      val parcelDeliveryType: ParcelDeliveryType
 ) : Ride(riderId, driverId, pickupLocation, dropLocation, fare) {
 
     init {
         require(parcelId.isNotBlank()){ "Parcel ID cannot be blank. "}
-        require(Validator.isValidName(contactName)) { "Contact name must contain minimum 3 characters. Contact name cannot be blank." }
-        require(Validator.isValidPhone(contactPhone)) { "Invalid contact phone number format." }
+        require(Validator.isValidName(contactPersonName)) { "Contact name must contain minimum 3 characters. Contact name cannot be blank." }
+        require(Validator.isValidPhone(contactPersonPhoneNumber)) { "Invalid contact phone number format." }
     }
 
 }
